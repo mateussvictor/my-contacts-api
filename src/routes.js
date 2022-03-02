8150b61cbeec7e0d4d10a400eb19c1e5d0cfa636
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-01 23:27:17 -0300

    feat: add route to show contact according to id

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const ContactController = require('./app/controllers/ContactController');
 const router = Router();
 
 router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
 
 module.exports = router;
