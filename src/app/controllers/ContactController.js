6b749cfd1977a824f18f88aa104f056a4903db9f
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-02 11:41:24 -0300

    feat: add store method on ContactController

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,6 +19,31 @@ class ContactController {
     response.json(contact);
   }
 
+  async store(request, response) {
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (contactExists) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
+  }
+
   async delete(request, response) {
     const { id } = request.params;
 
