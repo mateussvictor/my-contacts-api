57bb3479e24fa2fb3fd70f99ae92ab31d178d833
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-02 11:04:30 -0300

    feat: add route to delete contact

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
+router.delete('/contacts/:id', ContactController.delete);
 
 module.exports = router;
