bc8a7a2543e6abbf319d5ce55618293b7f804893
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-02 11:41:43 -0300

    feat: add route to create contact

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
+router.post('/contacts', ContactController.store);
 router.delete('/contacts/:id', ContactController.delete);
 
 module.exports = router;
