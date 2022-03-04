1a0c7dd77562896b099621921e21919902f043bc
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-03 22:44:26 -0300

    feat: add update method on ContactsRepository

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -50,6 +50,24 @@ class ContactsRepository {
     });
   }
 
+  update(id, { name, email, phone, category_id }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) =>
+        contact.id === id ? updatedContact : contact,
+      );
+
+      resolve(updatedContact);
+    });
+  }
+
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
