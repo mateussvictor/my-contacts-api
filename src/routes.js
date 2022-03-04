fac90eb334a59683437ab3a641b7c0e655790ace
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-03 22:45:22 -0300

    feat: add route to update contact info

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
 
 module.exports = router;
