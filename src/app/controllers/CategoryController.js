6c107d276c4619fe1fc922abb99bac1ae61b1b7c
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 19:04:34 -0300

    feat: add update method on CategoryController

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -30,6 +30,25 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async update(request, response) {
+    const { name } = request.body;
+    const { id } = request.params;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required ' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
 }
 
 module.exports = new CategoryController();
