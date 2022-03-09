5bb9da4fb5efc9bf2aab351e2f437f3df7131234
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 19:08:35 -0300

    feat: add route to delete category

--- src/routes.js
+++ src/routes.js
@@ -14,5 +14,6 @@ router.get('/categories', CategoryController.index);
 router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
 router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
