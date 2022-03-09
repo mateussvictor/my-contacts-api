3f78739743343e31c1d686c0767b7f474f3a1e9f
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 19:40:06 -0300

    feat: add error handler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+
+  response.sendStatus(500);
+});
 
 app.listen(3000, () =>
   console.log('Server is running at http://localhost:3000'),
