36698432611defbe25122b68c8f35acf8d93c4a5
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 16:15:10 -0300

    feat: add query to sort contacts

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -2,7 +2,9 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response) {
-    const contacts = await ContactsRepository.findAll();
+    const { orderBy } = request.query;
+
+    const contacts = await ContactsRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -77,12 +79,6 @@ class ContactController {
 
   async delete(request, response) {
     const { id } = request.params;
-
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
-    }
 
     await ContactsRepository.delete(id);
     response.sendStatus(204);
