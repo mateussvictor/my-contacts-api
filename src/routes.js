371f8a51098dad94809edea014a514a322330a50
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 19:08:07 -0300

    feat: add route to update category

--- src/routes.js
+++ src/routes.js
@@ -13,5 +13,6 @@ router.delete('/contacts/:id', ContactController.delete);
 router.get('/categories', CategoryController.index);
 router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
