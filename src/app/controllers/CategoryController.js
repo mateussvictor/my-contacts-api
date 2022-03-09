e43a90985c90837efc0561ac5edfc43ac6d1af14
Mateus Victor <mateusvictor.dev@gmail.com>
2022-03-09 19:03:43 -0300

    feat: add show method on CategoryController

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,18 @@ class CategoryController {
     response.json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
