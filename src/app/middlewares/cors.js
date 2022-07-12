f677834ed3eb67fd7ab20f609f360430c45ebbcd
Mateus Victor <mateusvictor.dev@gmail.com>
2022-07-12 18:17:55 -0300

    feat: set headers

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,4 +1,8 @@
 module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
+
   next();
 };
