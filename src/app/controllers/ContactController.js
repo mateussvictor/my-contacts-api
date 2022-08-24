e00b1f0d08b866abd4caa6324370ba429ad59931
Mateus Victor <mateusvictor.dev@gmail.com>
2022-08-24 15:25:00 +0100

    refactor: improve email validation

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -14,7 +14,7 @@ class ContactController {
     const { id } = request.params;
 
     if (!isValidUUID(id)) {
-      return response.status(400).json({ error: 'Invalid user id' });
+      return response.status(400).json({ error: 'Invalid contact id' });
     }
 
     const contact = await ContactsRepository.findById(id);
@@ -61,7 +61,7 @@ class ContactController {
     const { name, email, phone, category_id } = request.body;
 
     if (!isValidUUID(id)) {
-      return response.status(400).json({ error: 'Invalid user id' });
+      return response.status(400).json({ error: 'Invalid contact id' });
     }
 
     if (category_id && !isValidUUID(category_id)) {
@@ -99,7 +99,12 @@ class ContactController {
   async delete(request, response) {
     const { id } = request.params;
 
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid contact id' });
+    }
+
     await ContactsRepository.delete(id);
+
     response.sendStatus(204);
   }
 }
