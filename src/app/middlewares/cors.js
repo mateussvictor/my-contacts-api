734f10b7e7b61f0e4f7e84ec25016792b3827ccf
mateus.victor <mateusvictor.dev@gmail.com>
2023-03-09 20:11:57 +0000

    refactor: allow different origins in cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,8 +1,15 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const allowedOrigins = ['http://localhost:3000'];
+
+  const origin = request.header('origin');
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
 
   next();
 };
